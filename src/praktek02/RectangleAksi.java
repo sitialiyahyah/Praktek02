f38c3f45f9be2e657d6208711a62771b533e8981
sitialiyahyah <sitialiyah94.net@gmail.com>
2023-05-25 14:18:24 +0800

    Mengisi nilai atribute

--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -2,7 +2,9 @@ package praktek02;
 
 public class RectangleAksi {
     public static void main(String[] args) {
-        Rectangle rl = new Rectangle();        
+        Rectangle rl = new Rectangle();
+        rl.panjang = 9;
+        rl.lebar = 3;
     }
                 
 }
